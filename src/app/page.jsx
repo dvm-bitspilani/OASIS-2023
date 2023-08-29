6736d9d233b93e8a345fd77e7181f3511f183b4f
Ritikchandra <ritik.chandra009@gmail.com>
2023-08-29 20:28:01 +0530

    Initial Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,18 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 
 import React from "react";
 import { HamContext } from "@/context/HamContextProvider";
 
 export default function Home() {
-  const { isHamOpen, setIsHamOpen } = React.useContext(HamContext);
+  // const { isHamOpen, setIsHamOpen } = React.useContext(HamContext);
 
   return (
     <main>
-      <button onClick={() => setIsHamOpen((prev) => !prev)}>
-        Change state
-      </button>
-      <br />
-      {!isHamOpen && "hello"}
+      <div className="reg-btn-wrapper">
+        
+      </div>
     </main>
   );
 }
