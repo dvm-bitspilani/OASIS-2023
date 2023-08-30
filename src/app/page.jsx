b682623af83f1dfd31f9d5d742f15cc05a05da28
Ritikchandra <ritik.chandra009@gmail.com>
2023-08-30 20:23:00 +0530

    Fixed: landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,26 @@ import styles from "./page.module.css";
 import Image from 'next/image'
 import React from "react";
 import { HamContext } from "@/context/HamContextProvider";
-
+import { useState, useEffect } from "react";
 export default function Home() {
   // const { isHamOpen, setIsHamOpen } = React.useContext(HamContext);
-  if (typeof window !== 'undefined') {
-    textLogoWidth = Math.floor(window.innerWidth * 0.3);
-    textLogoHeight = Math.floor(window.innerHeight * 0.2);
-    LandingBookWidth = Math.floor(window.innerWidth * 0.5);
-    LandingBookHeight = Math.floor(window.innerHeight * 0.5);
-    RegisterBtnWidth = Math.floor(window.innerWidth * 0.5);
-    RegisterBtnHeight = Math.floor(window.innerHeight * 0.5);
-  }
+  const [textLogoWidth, setTextLogoWidth] = useState(0);
+  const [textLogoHeight, setTextLogoHeight] = useState(0);
+  const [LandingBookWidth, setLandingBookWidth] = useState(0);
+  const [LandingBookHeight, setLandingBookHeight] = useState(0);
+  const [RegisterBtnWidth, setRegisterBtnWidth] = useState(200);
+  const [RegisterBtnHeight, setRegisterBtnHeight] = useState(75);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setTextLogoWidth(Math.floor(window.innerWidth * 0.3));
+      setTextLogoHeight(Math.floor(window.innerHeight * 0.2));
+      setLandingBookWidth(Math.floor(window.innerWidth * 0.5));
+      setLandingBookHeight(Math.floor(window.innerHeight * 0.5));
+      setRegisterBtnWidth(Math.min(200, Math.floor(window.innerWidth * 0.5)));
+      setRegisterBtnHeight(75);
+    }
+  }, []);
   return (
     <main>
      <div className={styles.pageWrapper}>
