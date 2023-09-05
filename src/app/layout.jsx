306b6c4ed24321d0f3f2178d2ab373ff0d24fb2b
Luv Gupta <lg5122003@gmail.com>
2023-09-05 14:18:14 +0530

    feat: add metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,41 @@
-"use client"
-
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-import Provider from "../context/Provider"
+import Provider from "../context/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
+export const metadata = {
+  title: {
+    default: "Oasis '23",
+  },
+  description: "The Official Website for OASIS 2023.",
+  colorScheme: "dark",
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      maximumScale: 1,
+    },
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>
-          {children}
-        </Provider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
