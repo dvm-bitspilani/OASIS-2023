c057fc785a0031d48a8cc03f3ba9cf0e26201b0d
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-09-06 21:16:09 +0530

    remove: console logs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -113,11 +113,11 @@ export default function Home() {
   const [allAssetsLoaded, setAllAssetsLoaded] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined") {
-      console.log('first')
+      // console.log('first')
       setIsLoading(true);
       setShowLoader(true);
       const assets = [textLogo, landingPgBookImg, rightElements, leftElements];
-        console.log('second')
+        // console.log('second')
       const loadAssets = () => {
         const assetPromises = assets.map((asset) => {
           if (asset) {
@@ -137,7 +137,7 @@ export default function Home() {
             setIsLoading(false);
             setShowLoader(false);
           }, 10000);
-          console.log('All assets loaded successfully');
+          // console.log('All assets loaded successfully');
         })
         .catch((error) => {
           console.error('Error loading assets:', error);
