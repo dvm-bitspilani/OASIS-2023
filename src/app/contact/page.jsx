d048367efd5775aac9e46f7a9a781c27ec316abc
Ritikchandra <ritik.chandra009@gmail.com>
2023-09-07 17:26:17 +0530

    modified: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+<<<<<<< Updated upstream
 import React, {useState} from 'react'
 import styles from "./page.module.css";
 import ImageWrapper from "../../../public/static/images/BookContact.png"
@@ -12,35 +13,128 @@ import ImageWrapper from "../../../public/static/images/BookContact.png"
 import Image from 'next/image';
 const Page = () => {
     const [imageSrc, setImageSrc] = useState(ImageWrapper);
+=======
+// import React, {useState} from 'react'
+// import styles from "./page.module.css";
+// import ImageWrapper from "../../../public/static/images/BookContact.png"
+// import Image from 'next/image';
+// import ContactProfile from '@/components/ContactProfile';
+// const page = () => {
+//     const [imageSrc, setImageSrc] = useState(ImageWrapper);
+//     const [hoveredProfile, setHoveredProfile] = useState(null);
+//   const handleMouseOver = (image, profile) => {
+//     setImageSrc(image);
+//     setHoveredProfile(profile);
+//   };
+//   const profiles = [
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//     { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+//   ];
+//   return (
+//     <>
+//     <div className={styles.pageWrapper}>
+//         <div className={styles.heading}>CONTACT US</div>
+//         <div className={styles.mainSection}>
+//             <div className={styles.department}>
+//             {/* <p>Registration, Events & Approval Queries</p> */}
+//             {/* <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+//             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p> */}
+//             {profiles.map((profile, index) => (
+//               <p
+//                 key={index}
+//                 onMouseOver={() => handleMouseOver(ImageWrapper, profile)}
+//                 onMouseOut={() => setHoveredProfile(null)}
+//               >
+//                 {profile.dept}
+//               </p>
+//             ))}
+//             </div>
+//             <div className={styles.imgWrapper}>
+//                 <ContactProfile
+//               imageSrc={imageSrc}
+//               name={hoveredProfile ? hoveredProfile.name : ''}
+//               dept={hoveredProfile ? hoveredProfile.dept : ''}/>
+//                 </div>
+//         </div>
+//     </div>
+//     </>
+//   )
+// }
+>>>>>>> Stashed changes
 
-  const handleMouseOver = (image) => {
+// export default page
+
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import ImageWrapper from '../../../public/static/images/BookContact.png';
+import Image from 'next/image';
+import ContactProfile from '@/components/ContactProfile';
+
+const Page = () => {
+  const [imageSrc, setImageSrc] = useState(ImageWrapper);
+  const [hoveredProfile, setHoveredProfile] = useState(null);
+
+  const handleMouseOver = (image, profile) => {
     setImageSrc(image);
+    setHoveredProfile(profile);
   };
+
+  const profiles = [
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+    { name: 'Shivang Rai', dept: 'Registration, Events & Approval Queries' },
+
+    // Add more profiles here
+  ];
+
   return (
     <>
-    <div className={styles.pageWrapper}>
+      <div className={styles.pageWrapper}>
         <div className={styles.heading}>CONTACT US</div>
         <div className={styles.mainSection}>
-            <div className={styles.department}>
-            {/* <p>Registration, Events & Approval Queries</p> */}
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
-            </div>
-            <div className={styles.imgWrapper}>
-                <Image
-                src={imageSrc}
-                />
-            </div>
+          <div className={styles.department}>
+            {profiles.map((profile, index) => (
+              <p
+                key={index}
+                onMouseOver={() => handleMouseOver(ImageWrapper, profile)}
+                onMouseOut={() => setHoveredProfile(null)}
+              >
+                {profile.dept}
+              </p>
+            ))}
+          </div>
+          <div className={styles.imgWrapper}>
+            <ContactProfile
+              imageSrc={imageSrc}
+              name={hoveredProfile ? hoveredProfile.name : ''}
+              dept={hoveredProfile ? hoveredProfile.dept : ''}
+            />
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
+<<<<<<< Updated upstream
 export default Page;
+=======
+export default Page;
+>>>>>>> Stashed changes
