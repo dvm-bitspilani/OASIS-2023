3732a105a3fcd122736f067175f3865815c74f6b
GeekWolf007 <geeksingal@gmail.com>
2023-09-07 19:09:30 +0530

    regPage fixes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,10 @@
 "use client"
 import React, {useState} from 'react'
 import styles from "./page.module.css";
-import ImageWrapper from "../../../public/static/images/BookContact.png"
+import { motion } from "framer-motion";
+import ImageWrapper from "../../../public/static/images/contacts.png"
+import phone from "../../../public/static/images/phone.svg"
+import mail from "../../../public/static/images/mail.svg"
 // import ImageWrapper1 from "../../../public/static/images/navLogo.png"
 // import ImageWrapper2 from "../../../public/static/images/hamBG.png"
 // import ImageWrapper3 from "../../../public/static/images/Group3.png"
@@ -12,6 +15,7 @@ import ImageWrapper from "../../../public/static/images/BookContact.png"
 import Image from 'next/image';
 const Page = () => {
     const [imageSrc, setImageSrc] = useState(ImageWrapper);
+    const [isLoading, setIsLoading] = useState(false);
 
   const handleMouseOver = (image) => {
     setImageSrc(image);
@@ -32,11 +36,32 @@ const Page = () => {
             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
             <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
             </div>
-            <div className={styles.imgWrapper}>
+            <motion.div className={styles.imgWrapper}
+              initial={{
+                opacity: 0,
+                transform: "scale(1) translateX(0) translateY(0) rotate(0deg)",
+              }}
+              animate={{
+                opacity: isLoading ? 0 : 1,
+                transform: isLoading
+                  ? "scale(1) translateX(0) translateY(0) rotate(0)"
+                  : "scaleX(.9) translateX(-8rem) translateY(-3rem) rotate(-0deg)",
+              }}
+              transition={{ ease: "easeOut", duration: 2 }}
+            >
                 <Image
                 src={imageSrc}
                 />
-            </div>
+                <div className="details">
+                  <Image />
+                  <h1></h1>
+                  <h2></h2>
+                  <div className="icons">
+                    <Image src={phone} />
+                    <Image src={mail} />
+                  </div>
+                </div>
+            </motion.div>
         </div>
     </div>
     </>
