a3e4b42a647a71b35f35b462b000ac030599ec24
Ritikchandra <ritik.chandra009@gmail.com>
2023-09-07 14:54:30 +0530

    modified: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import styles from "./page.module.css";
 import ImageWrapper from "../../../public/static/images/BookContact.png"
+// import ImageWrapper1 from "../../../public/static/images/navLogo.png"
+// import ImageWrapper2 from "../../../public/static/images/hamBG.png"
+// import ImageWrapper3 from "../../../public/static/images/Group3.png"
+// import ImageWrapper4 from "../../../public/static/images/Group2.png"
+// import ImageWrapper5 from "../../../public/static/images/Group1.png"
+// import ImageWrapper6 from "../../../public/static/images/LandingPageBook.png"
+// import ImageWrapper7 from "../../../public/static/images/Library.png"
 import Image from 'next/image';
 const page = () => {
+    const [imageSrc, setImageSrc] = useState(ImageWrapper);
+
+  const handleMouseOver = (image) => {
+    setImageSrc(image);
+  };
   return (
     <>
     <div className={styles.pageWrapper}>
         <div className={styles.heading}>CONTACT US</div>
         <div className={styles.mainSection}>
             <div className={styles.department}>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
-            <p>Registration, Events & Approval Queries</p>
+            {/* <p>Registration, Events & Approval Queries</p> */}
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
+            <p onMouseOver={() => handleMouseOver(ImageWrapper)}>Registration, Events & Approval Queries</p>
             </div>
             <div className={styles.imgWrapper}>
                 <Image
-                src={ImageWrapper}
+                src={imageSrc}
                 />
             </div>
         </div>
