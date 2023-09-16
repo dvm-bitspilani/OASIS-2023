7c712f398bcbe02b0e0519e12b813953e1fcee2b
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-09-16 23:29:11 +0530

    modify: events mobile

--- src/components/EventItemMobile.jsx
+++ src/components/EventItemMobile.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import * as events from "./eventsmobile.module.css";
-import Item from "../../public/static/images/StreetDance.png";
-import scrollAsset from "../../public/static/images/EventItem.png";
 
 const EventItem = ({ name, desc, image, top, left }) => {
   return (
@@ -13,14 +11,8 @@ const EventItem = ({ name, desc, image, top, left }) => {
         </div>
         <div className={events.itemContent}>
           <h2>{name}</h2>
-          {/* <p>
-            {desc}
-          </p> */}
         </div>
       </div>
-      {/* <div className={events.itemData}>
-          <Image draggable={false} alt="" src={scrollAsset} height={1200} width={840} />
-      </div> */}
     </>
   );
 };
