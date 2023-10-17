4281f22b8ab23c30305c79b1ba45863b7fff4c6a
yoshicon <shreyaskgantayet908@gmail.com>
2023-10-18 00:44:55 +0530

    tweaked the custom trail

--- src/components/CustomTrail.jsx
+++ src/components/CustomTrail.jsx
@@ -4,7 +4,7 @@ import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
 
 export default function CustomTrail({ children }) {
     const trailProps = {
-        lineDuration: 15,
+        lineDuration: 3,
         lineWidthStart: 10,
         strokeColor: "#5DB3F1",
         lag: 0,
