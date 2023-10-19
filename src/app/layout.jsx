4b7358c0f0ca43f830f229c9cffc3a466bf4408e
Luv Gupta <lg5122003@gmail.com>
2023-10-19 21:07:22 +0530

    update: metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google";
 import Script from "next/script";
 import Provider from "../context/Provider";
 import CustomTrail from "../components/CustomTrail";
+import OasisLogo from "../../public/static/images/eventsModalOasisLogo.png"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: {
-    default: "Oasis '23",
-  },
+  title: "Oasis '23",
   description: "The Official Website for OASIS 2023.",
+  image: OasisLogo,
   colorScheme: "dark",
   robots: {
     index: true,
