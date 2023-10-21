7fc3301cc2266b7e6ad2952dbadd5cc6b9a5d497
Ritikchandra <ritik.chandra009@gmail.com>
2023-10-22 00:41:15 +0530

    modified: events modal screen

--- src/components/GalleryCarousel.jsx
+++ src/components/GalleryCarousel.jsx
@@ -34,7 +34,7 @@ const GalleryCarousel = () => {
         keyboard = {true}
         mousewheel = {true}
         spaceBetween={30}
-        modules={[Grid, Navigation, Keyboard, Mousewheel]}
+        modules={[Grid, Navigation, Keyboard, Mousewheel]}  
         className="mySwiper"
       >
         <SwiperSlide></SwiperSlide>
@@ -94,3 +94,54 @@ const GalleryCarousel = () => {
 
 export default GalleryCarousel
 
+// import React, { useEffect, useState } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/grid';
+// import { Grid } from 'swiper/modules';
+// import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
+
+
+// const GalleryCarousel = () => {
+//   const [images, setImages] = useState([]);
+
+//   useEffect(() => {
+//     // Function to fetch images from the API
+//     const fetchImages = async () => {
+//       try {
+//         const response = await fetch('https://bits-oasis.org/2023/main/registrations/gallery/');
+//         const data = await response.json();
+//         setImages(data); // Assuming the API response is an array of image URLs
+//       } catch (error) {
+//         console.error('Error fetching images:', error);
+//       }
+//     };
+
+//     // Call the fetchImages function
+//     fetchImages();
+//   }, []); // Empty dependency array ensures useEffect runs once after initial render
+
+//   return (
+//     <>
+//       <Swiper
+//         slidesPerView={5}
+//         grid={{
+//           rows: 2,
+//         }}
+//         spaceBetween={30}
+//         modules={[Grid, Navigation, Keyboard, Mousewheel]}
+//         className="mySwiper"
+//       >
+//         {images.map((image, index) => (
+//           <SwiperSlide key={index}>
+//             <img src={image.picture} alt={`Slide ${index + 1}`} width={200} height={200}/>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </>
+//   );
+// };
+
+// export default GalleryCarousel;
